245dd2ab146fe8b6243fa90ed86356fbc529463b
piyushmendhe <mendhe.piyush31@gmail.com>
2024-01-09 21:32:41 +0530

    Array Questions

---
+++ LoveBabbarSheet/Arrays/Value_equal_to_index_value.java
@@ -0,0 +1,30 @@
+package LoveBabbarSheet.Arrays;
+
+import java.util.ArrayList;
+
+public class Value_equal_to_index_value {
+
+        public ArrayList<Integer> Solution(int arr[] , int n)
+        {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+            for(int i =0 ; i<n ;i++)
+            {
+                if(i+1==arr[i]) list.add(i+1);
+            }
+            System.out.println(list);
+            return list;
+        }
+        
+public static void main(String [] args)
+{
+    Value_equal_to_index_value result = new Value_equal_to_index_value();
+    int N = 5;
+    int Arr[] = {15, 2, 45, 12, 7};
+    result.Solution(Arr,N);
+
+
+}
+
+
+}
