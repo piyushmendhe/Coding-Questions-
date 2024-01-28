8fb707379533bce12b499f81edd9c4630ef529f1
piyushmendhe <mendhe.piyush31@gmail.com>
2024-01-28 17:13:03 +0530

    new queswtion

---
+++ LoveBabbarSheet/Arrays/MajorityElement.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class MajorityElement {
+
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i :a)
+        {
+                map.put(i,map.getOrDefault(i,0)+1);
+            
+            
+        }
+        for(int num :map.keySet())
+        {
+            if(map.get(num)>size/2)
+            {
+                return num;
+            }
+        }
+        return -1;
+    }
+    
+}
