4457c8c7546f0bac20dfbf9fc4b2963c15e11e5c
piyushmendhe <mendhe.piyush31@gmail.com>
2024-01-30 21:10:05 +0530

    portfolio

---
+++ LoveBabbarSheet/String/LCS_of_three_strings.java
@@ -0,0 +1,24 @@
+
+public class LCS_of_three_strings {
+
+    int LCSof3(String A, String B, String C, int n1, int n2, int n3) {
+        int[][][] dp = new int[n1 + 1][n2 + 1][n3 + 1];
+
+        // Initialize the dp table
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) {
+                for (int k = 0; k <= n3; k++) {
+                    if (i == 0 || j == 0 || k == 0)
+                        dp[i][j][k] = 0;
+                    else if (A.charAt(i - 1) == B.charAt(j - 1) && A.charAt(i - 1) == C.charAt(k - 1))
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    else
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                }
+            }
+        }
+
+        return dp[n1][n2][n3];
+    }
+    
+}
